80121382e5cfec11f764e17760d1a8bf44314f95
JonathanClark11 <JonathanClark11@gmail.com>
2023-01-22 16:48:38 -0800

    Folder structure

---
+++ src/util/HelperMethods.java
@@ -0,0 +1,11 @@
+public class HelperMethods {
+    public static String preMr(String name) {
+        return "Mr. " + name;
+    }
+    public static String preMs(String name) {
+        return "Ms. " + name;
+    }
+    public static String preDr(String name) {
+        return "Dr. " + name;
+    }
+}
