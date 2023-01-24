663569bc15bcb92f3c7f47c85e1082aaee430b87
Refactory.ai <hello@refactory.ai>
2023-01-23 22:21:39 -0800

    Adding refactory generated file: output/src/util/HelperMethods.test.java

---
+++ output/src/util/HelperMethods.test.java
@@ -0,0 +1,20 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelperMethodsTest {
+    @Test
+    public void testPreMr() {
+        assertEquals("Mr. John Smith", HelperMethods.preMr("John Smith"));
+    }
+    
+    @Test
+    public void testPreMs() {
+        assertEquals("Ms. Jane Doe", HelperMethods.preMs("Jane Doe"));
+    }
+    
+    @Test
+    public void testPreDr() {
+        assertEquals("Dr. John Doe", HelperMethods.preDr("John Doe"));
+    }
+}
